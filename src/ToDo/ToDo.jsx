ad4a48b8825e17726ed837cd3f286d463d0909a5
Amanda Stern <amanda.stern.73@gmail.com>
2023-03-03 15:12:56 -0500

    set state for edit tasks

--- src/ToDo/ToDo.jsx
+++ src/ToDo/ToDo.jsx
@@ -1,21 +1,64 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ToDo = (props) => {
 
   const { task, removeToDo, id } = props
 
+  const [isEditing, setIsEditing] = useState(false)
+  
+  const [editTask, setEditTask] = useState(task)
+
   function handleRemove(){
     removeToDo(id)
   }
+  
+  function toggleForm(){
+    setIsEditing(!isEditing)
+  }
 
-  return ( 
-    <>
+  function handleUpdate(evt){
+    evt.preventDefault()
+    // take new task data and pass up to parent
+    
+  }
+
+  function handleChange(evt){
+    setEditTask(evt.target.value)
+    console.log('this is evt.target.value', evt.target.value)
+    console.log('this is editTask', editTask)
+  }
+
+  let result
+
+  if (isEditing){
+    result = (
       <div>
-        <li>{task}</li>
-        <button>Edit</button>
+        <form onSubmit={handleUpdate}>
+          <label htmlFor="" name='editTask'>Edit To Do</label>
+          <input 
+            type="text" id="" 
+            value={editTask} 
+            name='editTask' 
+            onChange={handleChange}
+          />
+          <button>Save</button>
+        </form>
+      </div>
+    )
+  } else {
+    result = (
+      <div>
+        <li>{props.task}</li>
+        <button onClick={toggleForm}>Edit</button>
         <button onClick={handleRemove}>X</button>
       </div>
-    </>
+    )
+  }
+
+  return ( 
+    <div>
+      {result}
+    </div>
   );
 }
 
